5b823392ecac1ab367ad8d0fca5732ddd9ecbfa3
eveil <erick@erickveil.net>
2019-10-15 10:29:09 -0700

    Added Civilization class

---
+++ Civilization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darkmoor
+{
+    /// <summary>
+    /// Civilizations are made up of populations of Ancestries.
+    /// The populations are unified under a Name, though their Population.Rank
+    /// may vary.
+    /// </summary>
+    class Civilization
+    {
+        public string Name = "Unnamed";
+        public List<Population> CitizenList = new List<Population>();
+    }
+}
