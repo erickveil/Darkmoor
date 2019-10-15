9535313436cef36e493328b97d8967dbc33c2bfa
eveil <erick@erickveil.net>
2019-10-15 11:04:21 -0700

    Established Lairs and Hexes

---
+++ HexData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darkmoor
+{
+    /// <summary>
+    /// HexData is the information about a map hex.
+    /// Hexes are considered to be 12 miles wide from face to face, have 6 sides,
+    /// and contain 132 one-mile hexes within them.
+    /// Hexes also contain 0-5 Lairs, each located within a specific one-mile 
+    /// hex.
+    /// </summary>
+    class HexData
+    {
+        public List<Lair> LairList = new List<Lair>();
+        public int XCoord = 0;
+        public int YCoord = 0;
+
+    }
+}
---
+++ Lair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darkmoor
+{
+    /// <summary>
+    /// The home of a civilization.
+    /// Only one Civilization can live in a lair at a time.
+    /// If another one comes to live here, they must defeat and drive out the
+    /// existing occupants, assimilate them, or be assimilated in order to 
+    /// stay.
+    /// </summary>
+    class Lair
+    {
+        string Name = "Unnamed";
+        string Type = "Cave";
+        Civilization HomeCiv;
+    }
+}
